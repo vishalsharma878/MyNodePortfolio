1715857f5e79aa2ef4919bee7b369b98f3ffd571
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-26 10:59:39 +0530

    URL requests and return responses

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log("Vishal Sharma");
+    
+     const url = req.url;
+     if(url === "/home"){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Welcome Home</h1></body>');
+    res.write('</html>');
+    res.end();
+
+     }
+     if(url === "/about"){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title><head>');
+        res.write('<body><h1>Welcome to the about us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    
+         }
+         if(url === "/node"){
+            res.setHeader('Content-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>My First Page</title><head>');
+            res.write('<body><h1>Welcome to my node js project</h1></body>');
+            res.write('</html>');
+            res.end();
+             }
+    
+
 });
 
-server.listen(4000);
+server.listen(3000);
