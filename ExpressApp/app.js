d1f3810fc5024a1d47f1858383a944debe595299
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-27 14:24:48 +0530

    Added form in product page

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -1,13 +1,21 @@
 
 const express = require('express') ;
 const app = express();
+const bodyParser = require('body-parser')
 
-app.use((req, res, next) => {
-    console.log("This is Midleware");
-    next();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next) =>{
+    res.send('<form action="/product" method="POST"> <input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
 })
-app.use((req, res, next)=> {
-    res.send({"key": 4});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+app.use('/', (req, res, next)=> {
+   res.send('<h1>Hello from Express js</h1>');
+   
 })
 
 app.listen(3000);
