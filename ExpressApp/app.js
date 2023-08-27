95583b0116bebd0bfea94884d7fec62d052d6b24
Vishal Sharma <vishalsharma28784@gmail.com>
2023-08-27 16:41:20 +0530

    Added routes separately

--- ExpressApp/app.js
+++ ExpressApp/app.js
@@ -1,21 +1,16 @@
-
 const express = require('express') ;
 const app = express();
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use('/add-product', (req, res, next) =>{
-    res.send('<form action="/product" method="POST"> <input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
-})
+app.use('/admin',adminRoutes);
+app.use('/shop', shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-app.use('/', (req, res, next)=> {
-   res.send('<h1>Hello from Express js</h1>');
-   
+app.use((req, res, next) =>{
+    res.status(404).send('<h2>Page not found</h2>')
 })
 
 app.listen(3000);
---
+++ ExpressApp/routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/add-product', (req, res, next) =>{
+    res.send('<form action="/admin/add-product" method="POST"> <input type="text" name="title"><input type="number" name="size"><button type="submit">Add product</button></form>')
+})
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/shop/shop');
+})
+
+module.exports = router;
